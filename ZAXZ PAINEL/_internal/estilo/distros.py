22942fd2dfbd56c4a23df98b5a79d96110238b9c
D3LT4.1NCK <d3lt4.1nck@gmail.com>
2026-06-16 15:13:59 -0300

    adicionar ZAXZ PAINEL

---
+++ ZAXZ PAINEL/_internal/estilo/distros.py
@@ -0,0 +1,251 @@
+# Linux Mint
+modelo_mint = (
+    "Linux Mint",
+    "linux mint",
+    "LINUX MINT",
+    "LinuxMint",
+    "linuxmint",
+    "LINUXMINT",
+    "Mint",
+    "mint",
+    "MINT",
+    "Linux-Mint",
+    "linux-mint",
+    "LINUX-MINT",
+    "Linux Mint 22",
+    "linux mint 22",
+    "mint linux"
+)
+versao_segura_mint = 22.0
+
+
+# MX Linux
+modelo_mx = (
+    "MX Linux",
+    "mx linux",
+    "MX LINUX",
+    "MXLinux",
+    "mxlinux",
+    "MXLINUX",
+    "MX",
+    "mx",
+    "Mx",
+    "MX-Linux",
+    "mx-linux",
+    "MXLINUX",
+    "MX Linux 25",
+    "mx linux 25",
+    "linux mx"
+)
+versao_segura_mx = 23.0
+
+
+# Ubuntu
+modelo_ubuntu = (
+    "Ubuntu",
+    "ubuntu",
+    "UBUNTU",
+    "Ubuntu Linux",
+    "ubuntu linux",
+    "UBUNTU LINUX",
+    "Ubuntu LTS",
+    "ubuntu lts",
+    "UBUNTU LTS",
+    "Ubuntu Desktop",
+    "ubuntu desktop",
+    "Ubuntu Server",
+    "ubuntu server",
+    "Canonical Ubuntu",
+    "canonical ubuntu"
+)
+versao_segura_ubuntu = 24.04
+
+
+# Debian
+modelo_debian = (
+    "Debian",
+    "debian",
+    "DEBIAN",
+    "Debian GNU/Linux",
+    "debian gnu/linux",
+    "DEBIAN GNU/LINUX",
+    "Debian Linux",
+    "debian linux",
+    "Debian Stable",
+    "debian stable",
+    "DEBIAN STABLE",
+    "Debian GNU Linux",
+    "debian gnu linux",
+    "Debian 12",
+    "debian 12"
+)
+versao_segura_debian = 12.0
+
+
+# Fedora
+modelo_fedora = (
+    "Fedora",
+    "fedora",
+    "FEDORA",
+    "Fedora Linux",
+    "fedora linux",
+    "FEDORA LINUX",
+    "Fedora Workstation",
+    "fedora workstation",
+    "Fedora Server",
+    "fedora server",
+    "Fedora KDE",
+    "fedora kde",
+    "Fedora Project",
+    "fedora project",
+    "Fedora 43"
+)
+versao_segura_fedora = 43.0
+
+
+# Zorin
+modelo_zorin = (
+    "Zorin",
+    "zorin",
+    "ZORIN",
+    "Zorin OS",
+    "zorin os",
+    "ZORIN OS",
+    "ZorinOS",
+    "zorinos",
+    "ZORINOS",
+    "Zorin-OS",
+    "zorin-os",
+    "Zorin Desktop",
+    "zorin desktop",
+    "Zorin Linux",
+    "zorin linux"
+)
+versao_segura_zorin = 17.0
+
+
+# Elementary
+modelo_elementary = (
+    "elementary",
+    "Elementary",
+    "ELEMENTARY",
+    "elementary OS",
+    "Elementary OS",
+    "ELEMENTARY OS",
+    "elementaryOS",
+    "ElementaryOS",
+    "elementaryos",
+    "elementary-os",
+    "Elementary-OS",
+    "elementary linux",
+    "Elementary Linux",
+    "elementary desktop",
+    "Elementary Desktop"
+)
+versao_segura_elementary = 8.0
+
+
+# AlmaLinux
+modelo_alma = (
+    "AlmaLinux",
+    "almalinux",
+    "ALMALINUX",
+    "Alma Linux",
+    "alma linux",
+    "ALMA LINUX",
+    "AlmaLinux OS",
+    "almalinux os",
+    "AlmaLinux Server",
+    "almalinux server",
+    "Alma",
+    "alma",
+    "ALMA",
+    "Alma-Linux",
+    "alma-linux"
+)
+versao_segura_alma = 9.0
+
+
+# Rocky Linux
+modelo_rocky = (
+    "Rocky Linux",
+    "rocky linux",
+    "ROCKY LINUX",
+    "RockyLinux",
+    "rockylinux",
+    "ROCKYLINUX",
+    "Rocky",
+    "rocky",
+    "ROCKY",
+    "Rocky-Linux",
+    "rocky-linux",
+    "Rocky Linux Server",
+    "rocky linux server",
+    "Rocky Enterprise",
+    "rocky enterprise"
+)
+versao_segura_rocky = 10.0
+
+
+# Slackware
+modelo_slackware = (
+    "Slackware",
+    "slackware",
+    "SLACKWARE",
+    "Slackware Linux",
+    "slackware linux",
+    "SLACKWARE LINUX",
+    "Slackware GNU/Linux",
+    "slackware gnu/linux",
+    "Slackware Current",
+    "slackware current",
+    "Slackware Stable",
+    "slackware stable",
+    "Slackware64",
+    "slackware64",
+    "SLACKWARE64"
+)
+versao_segura_slackware = 15.0
+
+
+# NixOS
+modelo_nixos = (
+    "NixOS",
+    "nixos",
+    "NIXOS",
+    "Nix OS",
+    "nix os",
+    "NIX OS",
+    "NixOS Linux",
+    "nixos linux",
+    "Nix",
+    "nix",
+    "NIX",
+    "Nix-OS",
+    "nix-os",
+    "NixOS Stable",
+    "nixos stable"
+)
+versao_segura_nixos = 25.05
+
+# Kali Linux
+modelo_kali = (
+    "Kali Linux",
+    "kali linux",
+    "KALI LINUX",
+    "Kali GNU/Linux",
+    "kali gnu/linux",
+    "KALI GNU/LINUX",
+    "Kali",
+    "kali",
+    "KALI",
+    "Kali-Linux",
+    "kali-linux",
+    "Kali Rolling",
+    "kali rolling",
+    "Kali Rolling Release",
+    "kali rolling release",
+    "Kali Linux Rolling",
+    "kali linux rolling"
+)
+versao_segura_kali = 2025.8
